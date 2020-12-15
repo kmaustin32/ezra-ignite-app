c382ce34620530ec9a05465fa825b1034081ad3f
Keenan (עזרא) <reaper31292@gmail.com>
2020-12-15 14:25:48 +0200

    Added to home page and made global style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { startAnimation } from 'framer-motion';
 import React from 'react';
 //Componenets and Pages
 import Home from './pages/Home';
+//Style
+import GlobalStyle from './components/GlobalStyles';
 
 
 function App() {
 
     return (
         <div className="App">
+            <GlobalStyle />
             <Home />
         </div>
     );
---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,44 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        top: 0;
+        left: 0;
+        box-sizing: border-box;
+        
+    }
+    html {
+        &::-webkit-scrollbar {
+            width: .5rem;
+        }
+        &::-webkit-scrollbar-thumb {
+            background-color: darkgray;
+        }
+    }
+    body  {
+        font-family: 'Montserrat', sans-serif;
+        width: 100%;
+        color: #333;
+    }
+    h2 {
+        font-size: 2.75rem;
+        font-family: 'Abril Fatface', cursive;
+        font-weight: lighter;
+        color: #333;
+    }
+    h3 {
+        font-size: 1.3rem;
+        padding: .5rem;
+        color: #333;
+    }
+    p {
+        font-size: 1.2rem;
+        line-height: 200%;
+        color: #333;
+    }
+    a {
+        text-decoration: none;
+    }
+`;
+
+export default GlobalStyle;
